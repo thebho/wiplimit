7257a6999d2e39ec338c6c8443adb70d15f88bde
Brian Hoehne <brianhoehne@gmail.com>
2019-04-09 15:50:52 -0500

    add cleaner print statements and median/mode calculation

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -7,14 +7,14 @@ object Hello extends App {
 
   val mod1 = -1
   val mod2 = 0
-  val mod3 = 5
+  val mod3 = 1
   val mod4 = 2
-  val iterations: Int = 100
+  val iterations: Int = 10000
   println("WIP Limit App")
 
   val output = scala.collection.mutable.ListBuffer.empty[Int]
 
-  for (a <- 1 to 1000) {
+  for (a <- 1 to 10000) {
     var ui = 0
     var dev = 0
     var qa = 0
@@ -66,7 +66,10 @@ object Hello extends App {
     output.append(done - (ui + dev + qa))
 
   }
-println(output.max)
-println(output.min)
-println(output.sum/1000)
+  println(s"${mod1} | ${mod2} | ${mod3} | ${mod4}")
+  println(s"Mean:   ${output.sum/10000}")
+  println(s"Median: ${output.sortWith(_ < _)(5000)}")
+  println(s"Mode:   ${output.groupBy(i => i).mapValues(_.size).maxBy(_._2)._1}")
+  println(s"Max:    ${output.max}")
+  println(s"Min:    ${output.min}")
 }
